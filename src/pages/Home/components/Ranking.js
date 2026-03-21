de01f75e3a097c280f4d533378f1b204f6aa4083
tomassrato <tomassrato@gmail.com>
2026-03-21 06:26:50 +0000

    pequeno ajuste

--- src/pages/Home/components/Ranking.js
+++ src/pages/Home/components/Ranking.js
@@ -98,7 +98,7 @@ export default function Ranking() {
 						<p className="text-gray-400">A carregar...</p>
 					) : (
 						podioVisual.map((u) => (
-							<div key={u.id} className="flex flex-col items-center lg:flex-none">
+							<div key={u.id} className="flex flex-col items-center lg:flex-none flex-1">
 								{u.dadosUsuario?.imagemUrl ? (
 									<img
 										src={`${process.env.REACT_APP_API_URL}${u.dadosUsuario.imagemUrl}`}
@@ -108,7 +108,7 @@ export default function Ranking() {
 								) : (
 									<div className="w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] rounded-full bg-black mb-2" />
 								)}
-								<div className={`${u.gradiente} w-full max-w-[120px] lg:max-w-none lg:w-[240px] ${u.altura} flex flex-col text-center justify-start shadow-lg pt-4`}>
+								<div className={`${u.gradiente} w-full lg:w-[240px] ${u.altura} flex flex-col text-center justify-start shadow-lg pt-4`}>
 									<p className={`${u.cor} text-xl lg:text-2xl font-bold`}>{u.posicao}</p>
 									<p className={`${u.cor} text-sm lg:text-xl font-semibold break-words px-2`}>
 										{u.dadosUsuario?.alcunha || u.nome}
